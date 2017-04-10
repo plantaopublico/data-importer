26715f39e657583fba1f39d02e5d2c5690d1ecc9
Wagner Leonardi <leonardiwagner@gmail.com>
2017-04-10 17:46:28 -0300

    api

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-//const doctorsCsvImporter = require('./importers/doctors-ahmsp-importer')
+const fs = require('fs')
+const express = require('express')
+const app = express()
 
+app.get('/hospitals', (req, res) => {
+    const stat = fs.statSync('data/hospitals.json');
 
-//return doctorsCsvImporter('./assets/funcionarioshspm.csv')
-//.then((data) => console.log("DATA", data))
-//.catch((data) => console.log("DATA", data))
+    res.writeHead(200, {
+        'Content-Type': 'text/json',
+        'Content-Length': stat.size
+    })
+
+    const readStream = fs.createReadStream('data/hospitals.json');
+    readStream.pipe(res)
+})
+
+app.listen(3000)
